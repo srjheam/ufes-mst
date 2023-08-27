eca4ce54bc1a3a3cecfc3c885a75eb3cda3e6ec3
Jheam Storch Ross <jheam.ross@gmail.com>
2023-08-27 01:08:26 -0300

    Add mst 😱

---
+++ src/mstlib/include/mstlib/mst_serdes.h
@@ -0,0 +1,10 @@
+#ifndef _MSTLIB_MST_SERDES_
+#define _MSTLIB_MST_SERDES_
+
+#include <stdio.h>
+
+#include "mstlib/mst.h"
+
+void mstlib_mst_serialize(Mst *mst, FILE *fp);
+
+#endif
---
+++ src/mstlib/mst.c
@@ -0,0 +1,2 @@
+#include "mstlib/mst.h"
+
