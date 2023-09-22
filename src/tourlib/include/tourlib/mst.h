0ba7f38ed7ec2f272becd59df70b88d7f6d380d2
lsalatiel <lucassalatiel1004@gmail.com>
2023-09-21 22:32:51 -0300

    add comments

--- src/tourlib/include/tourlib/mst.h
+++ src/tourlib/include/tourlib/mst.h
@@ -6,18 +6,60 @@
 
 typedef struct Mst Mst;
 
+/**
+ * @brief Initialize a new MST.
+ * 
+ * @param name The name of the MST.
+ * @param dimension The dimension of the MST.
+ * @return Mst* The new MST.
+ */
 Mst *tourlib_mst_init(char *name, int dimension);
 
+/**
+ * @brief Add an edge to the MST.
+ * 
+ * @param mst The MST.
+ * @param id_u The id of the first vertex.
+ * @param id_v The id of the second vertex.
+ */
 void tourlib_mst_add_edge(Mst *mst, int id_u, int id_v);
 
+/**
+ * @brief Get the name of the MST.
+ * 
+ * @param mst The MST.
+ * @return char* The name of the MST.
+ */
 char *tourlib_mst_name(Mst *mst);
 
+/**
+ * @brief Get the dimension of the MST.
+ * 
+ * @param mst The MST.
+ * @return int The dimension of the MST.
+ */
 int tourlib_mst_dimension(Mst *mst);
 
+/**
+ * @brief Get the edges of the MST.
+ * 
+ * @param mst The MST.
+ * @return LinkedList** The edges of the MST.
+ */
 LinkedList **tourlib_mst_edges(Mst *mst);
 
+/**
+ * @brief Frees the MST.
+ * 
+ * @param mst The MST.
+ */
 void tourlib_mst_free(Mst *mst);
 
+/**
+ * @brief Print the MST.
+ * 
+ * @param mst The MST.
+ */
 void tourlib_mst_print(Mst *mst);
 
 #endif
--- src/tourlib/include/tourlib/tour.h
+++ src/tourlib/include/tourlib/tour.h
@@ -5,16 +5,52 @@
 
 typedef struct Tour Tour;
 
+/**
+ * @brief Initialize a Tour object.
+ * 
+ * @param name The name of the tour.
+ * @param dimension The dimension of the tour.
+ * @return Tour* The initialized Tour object.
+ */
 Tour *tourlib_tour_init(char *name, int dimension);
 
+/**
+ * @brief Makes a DFS on the MST and stores the path in the tour strcture.
+ * 
+ * @param tour The tour.
+ * @param adjacency_list The edges of the MST.
+ */
 void tourlib_tour_path(Tour *tour, LinkedList **adjacency_list);
 
+/**
+ * @brief Get the name of the tour.
+ * 
+ * @param tour The tour.
+ * @return char* The name of the tour.
+ */
 char *tourlib_tour_name(Tour *tour);
 
+/**
+ * @brief Get the dimension of the tour.
+ * 
+ * @param tour The tour.
+ * @return int The dimension of the tour.
+ */
 int tourlib_tour_dimension(Tour *tour);
 
+/**
+ * @brief Get the vertexes of the tour.
+ * 
+ * @param tour The tour.
+ * @return int* The vertexes of the tour.
+ */
 int *tourlib_tour_vertexes(Tour *tour);
 
+/**
+ * @brief Frees the tour.
+ * 
+ * @param tour The tour to be freed.
+ */
 void tourlib_tour_free(Tour *tour);
 
 #endif
